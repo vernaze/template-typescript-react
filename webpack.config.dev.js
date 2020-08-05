c9eb76e5dc1f76bc52cf965c14c248ac75681cc8
vernaze <vernalaze@gmail.com>
2020-08-05 12:27:32 +0900

    add: necessary files

---
+++ webpack.config.dev.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './ts/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.html?$/,
+        exclude: /node_modules/,
+        use: 'html-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [ 'node_modules', path.resolve(__dirname, 'src/ts') ],
+    extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", ".json"],
+  },
+  devtool: 'source-map',
+  devServer: {
+    https: true,
+    inline: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    watchContentBase: true,
+    hot: true,
+    port: 8888
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ],
+  node: {
+    fs: 'empty'
+  }
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  context: path.resolve(__dirname, 'src'),
+  entry: './ts/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.html?$/,
+        exclude: /node_modules/,
+        use: 'html-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [ 'node_modules', path.resolve(__dirname, 'src/ts') ],
+    extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", ".json"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ],
+  node: {
+    fs: 'empty'
+  }
+};
